f5b2ee5c1b341aa05f84c08e978f43c0d9533922
prasadharbare <prasad1523@proton.me>
2024-10-30 07:42:38 +0000

    set to local storage from forage

--- src/Components/SingleTask.js
+++ src/Components/SingleTask.js
@@ -1,14 +1,13 @@
-export default function (text, isCompleted,id) {
+export default function (text, isCompleted, id) {
   const label = document.createElement("label");
   label.classList.add("label", "cursor-pointer");
   if (isCompleted) {
-    label.onclick=()=>console.log("hello");
-    
     label.classList.add("bg-slate-200", "rounded", "p-2");
   }
-  label.innerHTML = `<span class="label-text">${text}</span>
+  label.innerHTML = `
+  <span class="label-text">${text}</span>
   <input type="checkbox" ${
     isCompleted && "checked"
-  } class="checkbox rounded-2xl"id=${id} />`;
+  } class="checkbox rounded-2xl" id=${id} />`;
   return label;
 }
--- src/main.js
+++ src/main.js
@@ -1,51 +1,77 @@
 import "./index.css";
+import localforage from "localforage";
 import SingleTask from "./Components/SingleTask";
-import { titleCase } from "./utils";
+import { titleCase, randomID } from "./utils";
 
-const showYear = document.querySelector(".show-year");
-const inputEl = document.querySelector("[data-user-input]");
 const formEl = document.querySelector("[data-form]");
+const inputEl = document.querySelector("[data-user-input]");
 const taskContainerEl = document.querySelector("[data-task-container]");
-showYear.textContent = new Date().getFullYear();
-
-function renderTask() {
-  taskContainerEl.innerHTML = "";
-  const frag = document.createDocumentFragment();
-  tasks.forEach((task) => {
-    frag.appendChild(SingleTask(task.text, task.isCompleted));
-  });
-  taskContainerEl.appendChild(frag);
-}
 
 let state = [];
-console.log(state);
 
-function toggleCopmleted(id) {
-  state.map((task) => {
-    if (id === task.id) {
+localforage.setDriver(localforage.LOCALSTORAGE)
+
+function updateLocal() {
+  localforage.setItem("tasks",state)
+}
+
+function toggleCompleted(id) {
+  console.log(id);
+  state = state.map((task) => {
+    if (task.id === id) {
       return { ...task, isCompleted: !task.isCompleted };
     }
     return task;
   });
-  console.log(state);
-}  
+}
 
+localforage.getItem("tasks").then((data) => {
+  state = data || [];
+  renderTask();
+})
+
+
+//Mark:Render
+function renderTask() {
+  taskContainerEl.innerHTML = "";
+  const frag = document.createDocumentFragment();
+  state.forEach((task) => {
+    frag.appendChild(SingleTask(task.task, task.isCompleted, task.id));
+  });
+  taskContainerEl.appendChild(frag);
+}
 
 formEl.addEventListener("submit", (e) => {
   e.preventDefault();
   if (!inputEl.value) return;
+  if (inputEl === ":clearAll") return clearTasks();
   const newTask = {
     task: titleCase(inputEl.value),
     isCompleted: false,
-    id: state.length,
+    id: randomID(),
   };
   state.unshift(newTask);
+  // localforage.setItem("tasks", state)
   renderTask();
-
+  updateLocal();
   inputEl.value = "";
 });
+
 taskContainerEl.addEventListener("click", (e) => {
-  console.log(e.target.id);
+  toggleCompleted(e.target.id);
+  state.sort((a, b) => a.isCompleted - b.isCompleted);
+  // localforage.setItem("tasks", state);
+  updateLocal()
+  renderTask();
 });
-   
-   
+
+function clearTasks() {
+  state.length = 0;
+  localforage.setItem("tasks", state);
+  renderTask()
+  inputEl.value = "";
+
+}
+
+const showYear = document.querySelector(".show-year");
+showYear.textContent = new Date().getFullYear();
